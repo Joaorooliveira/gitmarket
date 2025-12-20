ba11924e5889b992dbf1a9632eeb2b4fe35be805
Joaorooliveira <joaovictorooliveira812@gmail.com>
2025-12-20 18:44:47 -0300

    feat: adiciona validador que verifica se ja existe o telefone informado ao criar cliente

--- src/main/java/com/product/api/gitmarket/domain/cliente/validacoes/criacao_completa/ValidadorTelefoneCadastrado.java
+++ src/main/java/com/product/api/gitmarket/domain/cliente/validacoes/criacao_completa/ValidadorTelefoneCadastrado.java
@@ -1,4 +1,21 @@
 package com.product.api.gitmarket.domain.cliente.validacoes.criacao_completa;
 
-public class ValidadorTelefoneCadastrado {
+import com.product.api.gitmarket.domain.cliente.ClienteRepository;
+import com.product.api.gitmarket.domain.cliente.dto.ClienteComUsuarioRequestDTO;
+import com.product.api.gitmarket.infra.exception.ValidacaoException;
+
+public class ValidadorTelefoneCadastrado implements ValidadorClienteUsuario {
+
+    private final ClienteRepository repository;
+
+    public ValidadorTelefoneCadastrado(ClienteRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public void validar(ClienteComUsuarioRequestDTO dados) {
+        if (repository.existsByTelefone(dados.telefone())) {
+            throw new ValidacaoException("Telefone ja cadastrado");
+        }
+    }
 }
