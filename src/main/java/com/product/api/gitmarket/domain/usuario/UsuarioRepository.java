c23c8f9a61bb7a4483c52ff9a5d9924162b5cedc
Joaorooliveira <joaovictorooliveira812@gmail.com>
2025-12-20 19:35:47 -0300

    fix(usuario-repository): ajusta metodo com nome errado

--- src/main/java/com/product/api/gitmarket/domain/usuario/UsuarioRepository.java
+++ src/main/java/com/product/api/gitmarket/domain/usuario/UsuarioRepository.java
@@ -1,8 +1,11 @@
 package com.product.api.gitmarket.domain.usuario;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
     boolean existsByLogin(String login);
+
+    UserDetails findByLogin(String login);
 }
