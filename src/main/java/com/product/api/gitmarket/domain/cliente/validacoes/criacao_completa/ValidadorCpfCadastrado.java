bae2ffb1cc78cba52406d342bcce9596fcff1f65
Joaorooliveira <joaovictorooliveira812@gmail.com>
2025-12-20 18:45:30 -0300

    feat: adiciona validador para verificar se cpf ja existe no banco

--- src/main/java/com/product/api/gitmarket/domain/cliente/validacoes/criacao_completa/ValidadorCpfCadastrado.java
+++ src/main/java/com/product/api/gitmarket/domain/cliente/validacoes/criacao_completa/ValidadorCpfCadastrado.java
@@ -1,4 +1,25 @@
 package com.product.api.gitmarket.domain.cliente.validacoes.criacao_completa;
 
-public class ValidadorCpfCadastrado {
+import com.product.api.gitmarket.domain.cliente.ClienteRepository;
+import com.product.api.gitmarket.domain.cliente.dto.ClienteComUsuarioRequestDTO;
+import com.product.api.gitmarket.infra.exception.ValidacaoException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ValidadorCpfCadastrado implements ValidadorClienteUsuario {
+
+
+    private final ClienteRepository clienteRepository;
+
+    public ValidadorCpfCadastrado(ClienteRepository clienteRepository) {
+        this.clienteRepository = clienteRepository;
+    }
+
+
+    @Override
+    public void validar(ClienteComUsuarioRequestDTO dados) {
+        if (clienteRepository.existsByCpf(dados.cpf())) {
+            throw new ValidacaoException("CPF ja da cadastrado");
+        }
+    }
 }
