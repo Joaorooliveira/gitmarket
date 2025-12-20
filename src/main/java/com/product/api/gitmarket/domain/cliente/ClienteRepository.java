8149b5eafc4110bfcfa16eaec322eb9831371474
Joaorooliveira <joaovictorooliveira812@gmail.com>
2025-12-19 22:16:45 -0300

    feat: adiciona cliente repository

--- src/main/java/com/product/api/gitmarket/domain/cliente/ClienteRepository.java
+++ src/main/java/com/product/api/gitmarket/domain/cliente/ClienteRepository.java
@@ -1,4 +1,8 @@
 package com.product.api.gitmarket.domain.cliente;
 
-public interface ClienteRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ClienteRepository extends JpaRepository<Cliente, UUID> {
 }
