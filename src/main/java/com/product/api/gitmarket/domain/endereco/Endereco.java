9160d4328c1c6513dc0b0fdfe3f6875ef29230eb
Joaorooliveira <joaovictorooliveira812@gmail.com>
2025-12-19 22:16:30 -0300

    feat: adiciona classe endereco

--- src/main/java/com/product/api/gitmarket/domain/endereco/Endereco.java
+++ src/main/java/com/product/api/gitmarket/domain/endereco/Endereco.java
@@ -1,4 +1,26 @@
 package com.product.api.gitmarket.domain.endereco;
 
+import jakarta.persistence.Embeddable;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Embeddable
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Endereco {
+
+    private String logradouro;
+    private String numero;
+    private String complemento;
+    @Pattern(regexp = "\\d{8}", message = "CEP deve conter apenas 8 números")
+    private String cep;
+    private String cidade;
+    private String uf;
+    private String bairro;
+
 }
