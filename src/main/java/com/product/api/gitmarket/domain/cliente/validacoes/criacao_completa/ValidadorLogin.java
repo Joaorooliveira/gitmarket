df89a072673289ff036bdb3e99c6c07c6059b2c4
Joaorooliveira <joaovictorooliveira812@gmail.com>
2025-12-20 18:45:05 -0300

    feat: adiciona interface validador

--- src/main/java/com/product/api/gitmarket/domain/cliente/validacoes/criacao_completa/ValidadorLogin.java
+++ src/main/java/com/product/api/gitmarket/domain/cliente/validacoes/criacao_completa/ValidadorLogin.java
@@ -1,12 +1,23 @@
 package com.product.api.gitmarket.domain.cliente.validacoes.criacao_completa;
 
 import com.product.api.gitmarket.domain.cliente.dto.ClienteComUsuarioRequestDTO;
+import com.product.api.gitmarket.domain.usuario.UsuarioRepository;
+import com.product.api.gitmarket.infra.exception.ValidacaoException;
 import org.springframework.stereotype.Component;
 
 @Component
-public class ValidadorEmail implements ValidadorClienteUsuario {
+public class ValidadorLogin implements ValidadorClienteUsuario {
+
+    private final UsuarioRepository usuarioRepository;
+
+    public ValidadorLogin(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
     @Override
     public void validar(ClienteComUsuarioRequestDTO dados) {
-        
+        if (usuarioRepository.existsByLogin(dados.login())) {
+            throw new ValidacaoException("Login já cadastrado");
+        }
     }
 }
