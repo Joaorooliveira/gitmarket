812ca0e73f26ab199ad40baf895bed152f0ae2a6
Joaorooliveira <joaovictorooliveira812@gmail.com>
2025-12-20 18:48:05 -0300

    feat: adiciona dto para cliente

--- src/main/java/com/product/api/gitmarket/domain/cliente/dto/ClienteRequestDTO.java
+++ src/main/java/com/product/api/gitmarket/domain/cliente/dto/ClienteRequestDTO.java
@@ -1,6 +1,9 @@
 package com.product.api.gitmarket.domain.cliente.dto;
 
+import com.product.api.gitmarket.domain.cliente.Cliente;
+import com.product.api.gitmarket.domain.endereco.Endereco;
 import com.product.api.gitmarket.domain.endereco.dto.EnderecoRequestDTO;
+import com.product.api.gitmarket.domain.usuario.Usuario;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -27,5 +30,16 @@ public record ClienteRequestDTO(
         @NotNull
         Long usuario_id
 
+
 ) {
+
+    public Cliente toEntity(Usuario usuario) {
+        Cliente cliente = new Cliente();
+        cliente.setCpf(this.cpf);
+        cliente.setNome(this.nome);
+        cliente.setTelefone(this.telefone);
+        cliente.setEndereco(new Endereco(this.endereco));
+        cliente.setUsuario(usuario);
+        return cliente;
+    }
 }
