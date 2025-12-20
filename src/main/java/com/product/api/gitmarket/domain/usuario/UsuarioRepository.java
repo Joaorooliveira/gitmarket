953658b059574c81f6d280b5d9e2d8fe54c81258
Joaorooliveira <joaovictorooliveira812@gmail.com>
2025-12-20 18:43:30 -0300

    feat(usuario-repository): adiciona metodo de busca no banco

--- src/main/java/com/product/api/gitmarket/domain/usuario/UsuarioRepository.java
+++ src/main/java/com/product/api/gitmarket/domain/usuario/UsuarioRepository.java
@@ -1,8 +1,8 @@
 package com.product.api.gitmarket.domain.usuario;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    UserDetails findByLogin(String login);
+
+    boolean existsByLogin(String login);
 }
