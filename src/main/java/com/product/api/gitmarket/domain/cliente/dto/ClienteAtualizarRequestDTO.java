9c9218274694ef44b37aef83642cddc515c312e0
Joaorooliveira <joaovictorooliveira812@gmail.com>
2025-12-22 11:33:13 -0300

    feat: adiciona dto para atualizar cliente

--- src/main/java/com/product/api/gitmarket/domain/cliente/dto/ClienteAtualizarRequestDTO.java
+++ src/main/java/com/product/api/gitmarket/domain/cliente/dto/ClienteAtualizarRequestDTO.java
@@ -1,4 +1,11 @@
 package com.product.api.gitmarket.domain.cliente.dto;
 
-public record ClienteAtualizarRequestDTO() {
+import com.product.api.gitmarket.domain.endereco.dto.EnderecoAtualizarRequestDTO;
+
+public record ClienteAtualizarRequestDTO(
+        String cpf,
+        String nome,
+        String telefone,
+        EnderecoAtualizarRequestDTO endereco
+) {
 }
