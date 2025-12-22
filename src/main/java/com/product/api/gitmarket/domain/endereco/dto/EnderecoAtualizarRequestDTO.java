38290cf60887191ae935c35fef4b3e38f69d1e1e
Joaorooliveira <joaovictorooliveira812@gmail.com>
2025-12-22 11:34:01 -0300

    feat: adiciona dto para atualizar endereco

--- src/main/java/com/product/api/gitmarket/domain/endereco/dto/EnderecoAtualizarRequestDTO.java
+++ src/main/java/com/product/api/gitmarket/domain/endereco/dto/EnderecoAtualizarRequestDTO.java
@@ -1,4 +1,25 @@
 package com.product.api.gitmarket.domain.endereco.dto;
 
-public record EnderecoAtualizarRequestDTO() {
+import jakarta.validation.constraints.Pattern;
+
+public record EnderecoAtualizarRequestDTO(
+        String logradouro,
+
+        String numero,
+
+        String complemento,
+
+
+        @Pattern(regexp = "\\d{8}", message = "CEP deve conter apenas 8 números")
+        String cep,
+
+
+        String cidade,
+
+
+        String uf,
+
+
+        String bairro
+) {
 }
