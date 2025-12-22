6e0fb15aa8bf372353e9779efd70e636c99e8210
Joaorooliveira <joaovictorooliveira812@gmail.com>
2025-12-22 11:33:43 -0300

    feat(endereco): adiciona metodo para atualizar endereco

--- src/main/java/com/product/api/gitmarket/domain/endereco/Endereco.java
+++ src/main/java/com/product/api/gitmarket/domain/endereco/Endereco.java
@@ -1,5 +1,6 @@
 package com.product.api.gitmarket.domain.endereco;
 
+import com.product.api.gitmarket.domain.endereco.dto.EnderecoAtualizarRequestDTO;
 import com.product.api.gitmarket.domain.endereco.dto.EnderecoRequestDTO;
 import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
@@ -31,4 +32,33 @@ public class Endereco {
                 endereco.uf(),
                 endereco.bairro());
     }
+
+    public Endereco(EnderecoAtualizarRequestDTO dados) {
+        this.atualizarInformacoes(dados);
+    }
+
+    public void atualizarInformacoes(EnderecoAtualizarRequestDTO dados) {
+        if (dados.logradouro() != null) {
+            this.logradouro = dados.logradouro();
+        }
+        if (dados.numero() != null) {
+            this.numero = dados.numero();
+        }
+        if (dados.complemento() != null) {
+            this.complemento = dados.complemento();
+        }
+        if (dados.cep() != null) {
+            this.cep = dados.cep();
+        }
+        if (dados.cidade() != null) {
+            this.cidade = dados.cidade();
+        }
+        if (dados.uf() != null) {
+            this.uf = dados.uf();
+        }
+        if (dados.bairro() != null) {
+            this.bairro = dados.bairro();
+        }
+    }
+
 }
